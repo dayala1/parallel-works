f6bb2bf213b5a4fad55b2f6692edd2363f988be0
Daniel Ayala <danielayalahernandez@hotmail.com>
2025-09-24 09:14:44 +0200

    Updated site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://parallel.works',
+  site: 'https://parallelworks.tech',
   output: 'static',
   integrations: [sitemap()],
   redirects:{
